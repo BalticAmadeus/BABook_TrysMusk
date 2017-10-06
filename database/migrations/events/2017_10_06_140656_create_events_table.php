43fdd9d33b091fec751b9c03069f4679b6d8df57
Nedas Šimkus <nedshmk@gmail.com>
2017-10-06 17:13:22 +0300

    Created tables

---
+++ database/migrations/events/2017_10_06_140656_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('groupId')->unsigned();
+            $table->foreign('groupId')->references('id')->on('groups');
+            $table->string('title', 50);
+            $table->dateTime('date');
+            $table->string('comment');
+            $table->string('location', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
