e967edd0ad75d4dab0e5cbe763eda3c7c61bdf41
Tadas Kontrimavicius <tadas.kont@gmail.com>
2017-10-09 13:06:57 +0300

    Bootstrap Datepicker.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
+@section('head')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.css"></script>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
+@endsection
+
 @section('content')
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
@@ -70,4 +80,10 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    $(function() {
+        $('#datetimepicker1').datetimepicker();
+    });
+</script>
 @endsection
