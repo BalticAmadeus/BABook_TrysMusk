daf4cb8e6f6c46bd365d4d5e8307a3357a4958b1
nedas <nedas@pulsetip.com>
2017-10-11 16:33:05 +0300

    Database Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('groups')->insert([
+            'name' => "Alus",
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Vardenis',
+            'email' => 'vardenis@mail.com',
+            'password' => 'password',
+            'remember_token' => 'belekas',
+        ]);
+
+        DB::table('events')->insert([
+            'groupId' => 1,
+            'userId' => 1,
+            'title' => 'Einam alaus',
+            'date' => '2017-11-12 19:00:00',
+            'comment' => 'Komentaras',
+            'location' => 'Lokacija'
+        ]);
     }
 }
