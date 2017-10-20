08d28566d526be3b642cd0fe02dea5ad02e7b26d
nedas <nedas@pulsetip.com>
2017-10-20 15:52:40 +0300

    minor fix

--- routes/api.php
+++ routes/api.php
@@ -17,26 +17,27 @@ Route::group(['middleware' => ['api']], function () {
     Route::post('auth/login', 'ApiController@login');
     Route::group(['middleware' => 'jwt.auth'], function () {
         Route::get('user', 'ApiController@getAuthUser');
+        Route::namespace('api')->group(function () {
+            Route::get('events', 'EventsController@index');
+            Route::get('events/{eventId}', 'EventsController@show');
+            Route::post('events', 'EventsController@store');
+            Route::put('events/{eventId}', 'EventsController@update');
+            Route::delete('events/{eventId}', 'EventsController@delete');
+
+
+            Route::get('userevent/{eventId}', 'AttendanceController@show');
+            Route::get('userevent/invitable/{eventId}', 'AttendanceController@invitable');
+            Route::post('userevent', 'AttendanceController@store');
+
+            Route::get('comments/{eventId}', 'CommentsController@show');
+            Route::post('comments/{eventId}', 'CommentsController@store');
+
+            Route::get('groups', 'GroupsController@index');
+        });
     });
 });
 
-Route::namespace('api')->group(function () {
-    Route::get('events', 'EventsController@index');
-    Route::get('events/{eventId}', 'EventsController@show');
-    Route::post('events', 'EventsController@store');
-    Route::put('events/{eventId}', 'EventsController@update');
-    Route::delete('events/{eventId}', 'EventsController@delete');
 
-
-    Route::get('userevent/{eventId}', 'AttendanceController@show');
-    Route::get('userevent/invitable/{eventId}', 'AttendanceController@invitable');
-    Route::post('userevent', 'AttendanceController@store');
-
-    Route::get('comments/{eventId}', 'CommentsController@show');
-    Route::post('comments/{eventId}', 'CommentsController@store');
-
-    Route::get('groups', 'GroupsController@index');
-});
 
 
 
