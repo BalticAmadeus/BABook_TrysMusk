ac72a6aa135821fdc9a6335ff001947c3287f623
nedas <nedas@pulsetip.com>
2017-10-25 15:09:28 +0300

    Chat working

--- src/components/Room/Room.vue
+++ src/components/Room/Room.vue
@@ -1,64 +1,111 @@
    <template>
-     <v-container fill-height>
-           <v-layout row wrap align-center>
-             <v-flex class="text-xs-center">
-               <h2>Chat!</h2>
-               <v-flex xs12 sm12>
-           <v-list>
-             <v-list-tile v-for="item in items" v-bind:key="item.name">
-               <v-list-tile-content>
-                 <v-list-tile-title> {{ item.name }} : {{ item.comment }}</v-list-tile-title>
-               </v-list-tile-content>
-             </v-list-tile>
-               <v-layout row align-center>
-             <v-flex xs10 sm10>
-               <v-text-field
-               name="input-7-1"
-               label="Label Text"
-               multi-line
-             ></v-text-field>
-         </v-flex>
-         <v-flex xs2 sm2>
-         <v-btn color="primary">Send</v-btn>
-     </v-flex>
-   </v-layout>
-           </v-list>
-       </v-flex>
-                 <v-btn
-                   @click="submit"
-                   :disabled="!valid"
-                 >
-                   submit
-                 </v-btn>
-                 <v-btn @click="clear">clear</v-btn>
-             </v-flex>
-           </v-layout>
-         </v-container>
+     <v-layout row>
+            <v-flex xs12 sm8 offset-sm2>
+                <v-card>
+                    <v-card-title>
+                        <v-flex class="text-xs-center">
+                            <v-btn round primary v-on:click="start" :disabled="disabled">
+                                Start discussion
+                            </v-btn>
+                        </v-flex>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-flex xs12 sm10 offset-sm1>
+                            <div id="discussion"></div>
+                        </v-flex>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-flex  xs12 sm10 offset-sm1>
+                            <v-form>
+ 
+                                <v-text-field
+                                        label="Message"
+                                        type="text"
+                                        id="message"
+                                        required
+                                ></v-text-field>
+                                <v-flex class="text-xs-right">
+                                    <v-btn id="sendMessage">Send</v-btn>
+                                </v-flex>
+ 
+                                <input type="hidden" id="displayname" />
+                            </v-form>
+                        </v-flex>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+</template>
 
-   </template>
-
-   <script>
-import auth from "../../js/auth.js";
-import router from "../../router/index.js";
+<script>
+import Vue from "vue";
 
 export default {
-  data: () => ({
-    items: [],
-    auth: auth
-  }),
+  data() {
+    return {
+      disabled: false,
+     
+    };
+  },
   methods: {
-    check: function() {
-      let token = localStorage.getItem("access_token");
-      if (!token) {
-        router.push("/login");
-      }
+    start: function() {
+      this.disabled = true
+      var connection = $.hubConnection("http://trycatch2017.azurewebsites.net");
+      var chatHubProxy = connection.createHubProxy("ChatHub");
+
+      chatHubProxy.on("broadcastMessage", function(name, message) {
+        var encodedName = $("<div />")
+          .text(name)
+          .html();
+        var encodedMsg = $("<div />")
+          .text(message)
+          .html();
+        // Add the message to the page.
+        $("#discussion").append(
+          "<li><strong>" +
+            encodedName +
+            "</strong>:&nbsp;&nbsp;" +
+            encodedMsg +
+            "</li>"
+        );
+      });
+
+      connection.start().done(function() {
+        var token = localStorage.getItem("access_token");
+
+        if (token != null) {
+          $.ajax({
+            url: "http://trycatch2017.azurewebsites.net/api/user",
+            type: "GET",
+            beforeSend: function(xhr, settings) {
+              xhr.setRequestHeader("Authorization", "Bearer " + token);
+            },
+            success: function(res) {
+              $("#displayname").val(res.name);
+              chatHubProxy.invoke(
+                "Send",
+                $("#displayname").val(),
+                " now connected!"
+              );
+              $("#message")
+                .val("")
+                .focus();
+            }
+          });
+
+          $("#sendMessage").on("click", function() {
+            chatHubProxy.invoke("Send", $("#displayname").val(), $("#message").val());
+            $("#message").empty();
+          });
+        }
+      });
     }
-  }
+  },
+
 };
 </script>
 
-   <style lang="css">
-textarea {
-  resize: none;
-}
+
+<style lang="css">
+
 </style>
